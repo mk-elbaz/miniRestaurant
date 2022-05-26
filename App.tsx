f1518aad46fcb83522c3ffc60ac1b26d537afcd2
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-05-26 23:03:12 +0200

    calendar

--- App.tsx
+++ App.tsx
@@ -1,30 +1,82 @@
 import landing from "./screens/landing";
 import reserve from "./screens/reserve";
-import React from "react";
+import React, { useCallback, useEffect, useState } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 const Stack = createNativeStackNavigator();
+import {
+	Image,
+	ImageBackground,
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
 
-const App = () => (
-	<NavigationContainer>
-		<Stack.Navigator>
-			<Stack.Screen
-				name="reserve"
-				component={reserve}
-				options={{
-					headerShown: false,
-				}}
-			/>
-			<Stack.Screen
-				name="Landing"
-				component={landing}
-				options={{
-					headerShown: false,
-				}}
-			/>
-		</Stack.Navigator>
-	</NavigationContainer>
-);
+const App = () => {
+	const [appIsReady, setAppIsReady] = useState(false);
 
+	useEffect(() => {
+		async function prepare() {
+			try {
+				// Keep the splash screen visible while we fetch resources
+				await SplashScreen.preventAutoHideAsync();
+				// Pre-load fonts, make any API calls you need to do here
+				await Font.loadAsync(Entypo.font);
+
+				// Artificially delay for two seconds to simulate a slow loading
+				// experience. Please remove this if you copy and paste the code!
+				await new Promise(resolve => setTimeout(resolve, 2000));
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				// Tell the application to render
+				setAppIsReady(true);
+			}
+		}
+		prepare();
+	});
+
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			// This tells the splash screen to hide immediately! If we call this after
+			// `setAppIsReady`, then we may see a blank screen while the app is
+			// loading its initial state and rendering its first pixels. So instead,
+			// we hide the splash screen once we know the root view has already
+			// performed layout.
+			await SplashScreen.hideAsync();
+		}
+	}, [appIsReady]);
+
+	if (!appIsReady) {
+		return null;
+	}
+
+	return (
+
+		<NavigationContainer>
+			<View onLayout={onLayoutRootView}>
+			</View>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="reserve"
+					component={reserve}
+					options={{
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen
+					name="Landing"
+					component={landing}
+					options={{
+						headerShown: false,
+					}}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
 export default App;
--- screens/reserve.tsx
+++ screens/reserve.tsx
@@ -6,6 +6,7 @@ import {
     Text,
     View,
 } from "react-native";
+import Form from 'react-native-form';
 import {
     IconButton,
     TextInput,
@@ -17,9 +18,13 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Entypo from '@expo/vector-icons/Entypo';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
+import DatePicker from 'react-native-datepicker';
+
 
 const Reserve = () => {
-    const [search, setSearch] = useState("");
+    const [date, setDate] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
     const [appIsReady, setAppIsReady] = useState(false);
 
     useEffect(() => {
@@ -28,22 +33,44 @@ const Reserve = () => {
 
     return (
         <View style={styles.container}>
+            <Form>
+            <DatePicker
+        style={{width: 200}}
+        date={date}
+        mode="date"
+        placeholder="select date"
+        format="YYYY-MM-DD"
+        minDate="2022-05-27"
+        maxDate="2023-06-01"
+        confirmBtnText="Confirm"
+        cancelBtnText="Cancel"
+        customStyles={{
+          dateIcon: {
+            position: 'absolute',
+            left: 0,
+            top: 4,
+            marginLeft: 0
+          },
+          dateInput: {
+            marginLeft: 36
+          }
+          // ... You can check the source to find the other keys.
+        }}
+        onDateChange={(date)=>setDate(date)}
+      />
+            </Form>
             <TextInput
                 placeholder="Name"
-                style={{
-                    width: 350,
-                    height: 25,
-                    padding: 12,
-                    borderTopRightRadius: 47,
-                    borderTopLeftRadius: 47,
-                    borderBottomLeftRadius: 47,
-                    borderBottomRightRadius: 47,
-                    borderWidth: 0.4,
-                    backgroundColor: "white",
-                    position: "absolute",
-                }}
-                value={search}
-                onChangeText={(text: string) => setSearch(text)}
+                style={styles.textInput}
+                value={name}
+                onChangeText={(text: string) => setName(text)}
+                underlineColor="transparent"
+                activeUnderlineColor="blue"
+            /><TextInput
+                placeholder="Email"
+                style={styles.textInput}
+                value={email}
+                onChangeText={(text: string) => setEmail(text)}
                 underlineColor="transparent"
                 activeUnderlineColor="blue"
             />
@@ -65,6 +92,18 @@ const styles = StyleSheet.create({
     button: {
         backgroundColor: "#f8f9fa",
         padding: 10,
+    },
+    textInput: {
+        width: 350,
+        height: 25,
+        padding: 12,
+        borderTopRightRadius: 47,
+        borderTopLeftRadius: 47,
+        borderBottomLeftRadius: 47,
+        borderBottomRightRadius: 47,
+        borderWidth: 0.4,
+        backgroundColor: "white",
+        // position: "absolute",
     },
 });
 export default Reserve;
