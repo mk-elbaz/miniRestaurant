2392cb45591bc1460e720898355752ad4b741a20
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-05-27 15:48:15 +0200

    mongo

--- server/index.js
+++ server/index.js
@@ -1,3 +1,54 @@
 const express = require('express');
 const app  = express();
+const { MongoClient } = require('mongodb');
 
+
+const MONGO_URI = 'mongodb+srv://baz:baz@cluster0.0bthm.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_DB_NAME = 'Reservations';
+let dbConnection;
+
+const connect = async () => {
+    try {
+      const client = await MongoClient.connect(MONGO_URI);
+      dbConnection = client.db(MONGO_DB_NAME);
+    } catch (e) {
+      throw new Error(`Could not establish database connection: ${e}`);
+    }
+  };
+  
+  const mongoClient = async collectionName => {
+    if (!dbConnection) {
+      await connect();
+    }
+    if (collectionName) {
+      return dbConnection.collection(collectionName);
+    }
+    return dbConnection;
+  };
+
+  const order = {
+      "name" : "aaaaaaa",
+      "email" : "aaaaaaa@aaaa.aaaaa",
+      "phone" : "01111",
+  }
+
+  
+app.listen(3000, function() {
+    insert(order);
+    console.log("[OK] = HTTP Server listening on: http://localhost:3000");
+  });
+
+  const insert = async (data)=>{
+            dbConnection = await mongoClient("reservations");
+            console.log(dbConnection)
+			if (!dbConnection) {
+				console.log("Error")
+			}
+			await dbConnection.insertOne(data).catch((err) => {
+				console.error(err);
+			});
+  };
+  
+  module.exports = {
+    mongoClient
+  };
